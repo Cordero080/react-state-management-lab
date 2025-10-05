ae6ff10a8241af01b0574b93b4e0f91d0074fdf8
Pablo Cordero <cordero080@gmail.com>
2025-10-05 18:13:24 -0400

    animations on zoom added and adjusted

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,24 @@
-
 import React, { useRef, useEffect } from 'react';
 import './Card.css';
 import shadowAnimation from '../assets/shadow-animation.mp4';
 import sharpshooterAnimation from '../assets/sharpshooter.mp4';
+import brawlerAnimation from '../assets/brawler.mp4';
+import scavengerAnimation from '../assets/scavenger.mp4';
+import trackerAnimation from '../assets/tracker.mp4';
+import engineerAnimation from '../assets/engineer.mp4';
+import infiltratorAnimation from '../assets/infiltrator.mp4';
+import leaderAnimation from '../assets/leader.mp4';
+import medicAnimation from '../assets/medic.mp4';
 
 const Card = ({ slayer, isCrew, onAliasClick, onButtonClick, isZoomed }) => {
   const sharpshooterRef = useRef(null);
+  const brawlerRef = useRef(null);
+  const scavengerRef = useRef(null);
+  const trackerRef = useRef(null);
+  const engineerRef = useRef(null);
+  const infiltratorRef = useRef(null);
+  const medicRef = useRef(null);
+  const leaderRef = useRef(null);
 
   useEffect(() => {
     if (isZoomed && slayer.role === 'Sharpshooter' && sharpshooterRef.current) {
@@ -43,6 +56,89 @@ const Card = ({ slayer, isCrew, onAliasClick, onButtonClick, isZoomed }) => {
           className="zoom-card-img"
           style={{ width: '160px', height: '160px', objectFit: 'cover', marginBottom: '12px' }}
         />
+
+      ) :  isZoomed && slayer.role === 'Brawler' ? (
+
+        <video
+        ref={brawlerRef}
+        src={brawlerAnimation}
+        autoPlay
+        loop
+        muted
+        playsInline
+        className='zoom-card-img'
+        style={{width: '160px', height: '160px', objectFit: 'cover', marginBottom: '12px'}}
+        />
+
+      ) : isZoomed && slayer.role === 'Scavenger' ? (
+        <video
+        ref={scavengerRef}
+        src={scavengerAnimation}
+        autoPlay
+        loop
+        muted
+        playsInline
+        className='zoom-card-img'
+        style ={{ width: '160px', height: '160px', objectFit:'cover',
+          marginBottom:'12px'}}
+        />
+
+      ) : isZoomed && slayer.role === 'Tracker' ? (
+        <video
+        ref={trackerRef}
+        src={trackerAnimation}
+        autoPlay
+        loop
+        muted
+        playsInline
+        className='zoom-card-img' style ={{ width: '160px', height: '160px', objectFit:'cover',
+          marginBottom:'12px'}}
+          />
+      ) : isZoomed && slayer.role === 'Engineer' ? (
+        <video
+        ref={engineerRef}
+        src={engineerAnimation}
+        autoPlay
+        loop
+        muted
+        playsInline
+        className='zoom-card-img'
+        style={{ width: '160px', height: '160px', objectFit: 'cover' }}
+        />
+      ) : isZoomed && slayer.role === 'Infiltrator' ? (
+        <video
+        ref={infiltratorRef}
+        src={infiltratorAnimation}
+        autoPlay
+        loop
+        muted
+        playsInline
+        className='zoom-card-img'
+        style={{ width: '160px', height: '160px', objectFit: 'cover' }}
+        />
+      ) : isZoomed && slayer.role === 'Leader' ? (
+        <video
+          ref={leaderRef}
+          src={leaderAnimation}
+          autoPlay
+          loop
+          alt="Leader"
+          muted
+          playsInline
+          className="zoom-card-img"
+          style={{ width: '160px', height: '160px', objectFit: 'cover', marginBottom: '12px' }}
+        />
+      ) : isZoomed && slayer.role === 'Medic' ? (
+        <video
+          ref={medicRef}
+          src={medicAnimation}
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="zoom-card-img"
+          style={{ width: '160px', height: '160px', objectFit: 'cover', marginBottom: '12px' }}
+        />
       ) : (
         <img src={slayer.img} alt={slayer.role} />
       )}
@@ -67,6 +163,5 @@ const Card = ({ slayer, isCrew, onAliasClick, onButtonClick, isZoomed }) => {
     </li>
   );
 };
-// ...existing code...
 
 export default Card;
